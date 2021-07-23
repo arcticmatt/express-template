6093bd2f1f1c8ad46041311c6fe0f12ee23cd7d0
Matt Lim <mlimcollege@gmail.com>
2021-07-22 23:19:20 -0700

    Add eslint

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,9 @@ const main = async () => {
   const port = Number(process.env.SERVER_PORT);
   const host = process.env.SERVER_HOST as string;
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
 
   app.listen(port, host, () => {
     console.log(`Example app listening at http://${host}:${port}`);
